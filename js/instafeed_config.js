95d0bf9ca76ec419ca2164cf233238fd08fe1c9e
Prashant Gaurav <prashantgaurav1994@gmail.com>
2019-01-22 00:47:10 +0530

    Instafeed
    Display your Instagram photos on your site.

---
+++ js/instafeed_config.js
@@ -0,0 +1,16 @@
+/*------------------------------------*
+ * Author : Prashant Gaurav           *
+ *------------------------------------*/
+const userFeed = new Instafeed({
+    get: 'user',
+    userId: 'InstaUserID',
+    clientId: 'YouClientID',
+    accessToken: 'InstaAccessToken',
+    resolution: 'standard_resolution',
+    template: '<a href="{{link}}" target="_blank" id="{{id}}"><img src="{{image}}" ></img></a>',
+    sortBy: 'most-recent',
+    limit: false,  // you can limit np of post ie 6
+    links: false
+});
+userFeed.run();
+
